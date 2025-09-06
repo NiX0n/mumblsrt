0754cfc7ce99d51ce8f3497c22058bc3842432bc
Null Character <BiggoronSword+GitHub@gmail.com>
2025-09-06 10:27:21 -0400

    Added: JSDoc comments

--- sql/model/Transcription.js
+++ sql/model/Transcription.js
@@ -2,24 +2,53 @@
 class TranscriptionBase
 {
     /**
+     * Primary Key (PK)
      * @type {number}
      */
     id;
 
     /**
+     * Attempt (FK)
      * @type {number}
      */
     attemptId;
 
     /**
+     * Only active subtitles will be used 
+     * in final srt file generation
      * @type {number}
      */
     isActive;
 
+    /**
+     * Timestamp (ms) at begin of subtitle
+     * @type {number}
+     */
     fromOffset;
+
+    /**
+     * Timestamp (ms) at end of subtitle
+     * @type {number}
+     */
     toOffset;
+
+    /**
+     * Subtitle text
+     * @type {string}
+     */
     text;
+
+    /**
+     * JSON-encoded token data
+     * Includes confidence metrics
+     * associated with each subtitle token
+     * @type {string}
+     */
     tokens;
 }
 
+/**
+ * Transcription of audio data into subtitle text.
+ * Each instance was generated and identified by whisper.cpp as a phrase
+ */
 module.exports = class Transcription extends require('./Mixin')(TranscriptionBase) {};
